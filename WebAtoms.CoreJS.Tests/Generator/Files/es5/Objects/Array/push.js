a08eed8480aa339ef284a5f6672541a1681ddabb
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-09-09 15:40:38 +0530

    wip

---
+++ WebAtoms.CoreJS.Tests/Generator/Files/es5/Objects/Array/push.js
@@ -0,0 +1,12 @@
+﻿var animals = ['pigs', 'goats', 'sheep'];
+var count = animals.push("cows");
+
+assert(count === 4, count);
+
+assert(animals[3] === "cows");
+
+count = animals.push('chickens', 'cats', 'dogs');
+
+assert(count === 7, count);
+
+assert(animals[5] === "cats");
