784a6a858b335c500cc5c4c91ec2751bcbcda41f
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-09-21 09:44:19 +0530

    Added proxy class, no support yet

---
+++ WebAtoms.CoreJS/Core/Proxy/JSProxy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebAtoms.CoreJS.Extensions;
+
+namespace WebAtoms.CoreJS.Core
+{
+    public class JSProxy : JSValue
+    {
+        readonly JSObject target;
+        protected JSProxy(JSObject target, JSObject handler) : base(null)
+        {
+            if (target == null || handler == null)
+            {
+                throw JSContext.Current.NewTypeError("Cannot create proxy with a non-object as target or handler");
+            }
+            this.target = target;
+        }
+
+        public override bool BooleanValue => target.BooleanValue;
+
+        internal override IEnumerable<JSValue> AllElements => throw new NotImplementedException();
+
+        public override JSBoolean Equals(JSValue value)
+        {
+            return target.Equals(value);
+        }
+
+        public override JSValue InvokeFunction(JSValue thisValue, params JSValue[] args)
+        {
+            return target.InvokeFunction(thisValue, args);
+        }
+
+        public override JSBoolean StrictEquals(JSValue value)
+        {
+            return target.StrictEquals(value);
+        }
+
+        public override JSValue TypeOf()
+        {
+            return target.TypeOf();
+        }
+
+        internal override KeyString ToKey()
+        {
+            return target.ToKey();
+        }
+
+        [Constructor]
+        public static JSValue Constructor(JSValue t, JSValue[] a)
+        {
+            var (f, s) = a.Get2();
+            return new JSProxy(f as JSObject, s as JSObject);
+        }
+    }
+}
