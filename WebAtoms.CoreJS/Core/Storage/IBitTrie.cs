bfa3b09c3712ae2a313649d38a826bb791ec97dd
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-09-24 09:50:14 +0530

    wip

---
+++ WebAtoms.CoreJS/Core/Storage/IBitTrie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace WebAtoms.CoreJS.Core.Storage
+{
+    internal interface IBitTrie<TKey, TValue>
+    {
+
+        void Save(TKey key, TValue value);
+
+        TValue GetOrCreate(TKey key, TValue value);
+
+    }
+}
