fa5b9371c560d806a842bcccf69c36907d6edcfa
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-10-03 16:57:30 +0530

    wip

---
+++ WebAtoms.CoreJS/Core/Generator/JSGeneratorFunction.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace WebAtoms.CoreJS.Core.Generator
+{
+    public delegate void JSGeneratorDelegate(JSGenerator generator, in Arguments a);
+
+    public class JSGenerator : JSObject
+    {
+        readonly JSGeneratorDelegate @delegate;
+        readonly Arguments a;
+        JSValue value;
+        public JSGenerator(JSGeneratorDelegate @delegate, Arguments a)
+        {
+            this.@delegate = @delegate;
+            this.a = a;
+        }
+
+        // wait by current thread...
+        AutoResetEvent yield;
+
+        // wait by generator thread...
+        AutoResetEvent wait;
+
+        public JSValue Next()
+        {
+            if (yield == null)
+            {
+                yield = new AutoResetEvent(false);
+                ThreadPool.QueueUserWorkItem(RunGenerator, this);
+            }
+            wait.Set();
+            yield.WaitOne();
+            return value;
+        }
+
+        internal JSValue Yield(JSValue value)
+        {
+            yield.Set();
+            this.value = value;
+            wait.WaitOne();
+            return this.value;
+        }
+
+        private static void RunGenerator(object state)
+        {
+            JSGenerator generator = state as JSGenerator;
+            generator.@delegate(generator, generator.a);
+        }
+    }
+
+    public class JSGeneratorFunction: JSFunction
+    {
+        readonly JSGeneratorDelegate @delegate;
+
+        public JSGeneratorFunction(JSGeneratorDelegate @delegate): base(JSFunction.empty)
+        {
+            this.@delegate = @delegate;
+        }
+
+
+        public override JSValue InvokeFunction(in Arguments a)
+        {
+            return new JSGenerator(@delegate, a);
+        }
+
+    }
+}
