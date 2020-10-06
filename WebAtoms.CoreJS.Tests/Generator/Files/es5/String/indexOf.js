db6d5b90dcd023db25ed38de94b20be388ec7cf5
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-10-06 20:35:32 +0530

    indexOf implementation added

---
+++ WebAtoms.CoreJS.Tests/Generator/Files/es5/String/indexOf.js
@@ -0,0 +1,5 @@
+﻿var a = "akash";
+assert.strictEqual(1, a.indexOf("k"));
+assert.strictEqual(2, a.indexOf("a", 2));
+
+assert.strictEqual(-1, a.indexOf(null));
--- WebAtoms.CoreJS/Core/String/JSStringPrototype.cs
+++ WebAtoms.CoreJS/Core/String/JSStringPrototype.cs
@@ -17,7 +17,7 @@ namespace WebAtoms.CoreJS.Core
         private static JSString AsJSString(this JSValue v, 
             [CallerMemberName] string helper = null)
         {
-            if (v.IsUndefined || v.IsNull)
+            if (v.IsNullOrUndefined)
                 throw JSContext.Current.NewTypeError($"String.prototype.{helper} called on null or undefined");
             if (v is JSString str)
                 return str;
@@ -28,7 +28,7 @@ namespace WebAtoms.CoreJS.Core
         private static string AsString(this JSValue v,
             [CallerMemberName] string helper = null)
         {
-            if (v.IsUndefined || v.IsNull)
+            if (v.IsNullOrUndefined)
                 throw JSContext.Current.NewTypeError($"String.prototype.{helper} called on null or undefined");
             return v.ToString();
         }
@@ -44,14 +44,12 @@ namespace WebAtoms.CoreJS.Core
         [Prototype("substring")]
         public static JSValue Substring(in Arguments a) 
         {
-            var j = a.This as JSString;
-            if (j == null)
-                return JSUndefined.Value;
+            var @this = a.This.AsString();
             if (!a.TryGetAt(0, out var start))
                 return a.This;
             if (!a.TryGetAt(1, out var length))
-                return new JSString(j.value.Substring(start.IntValue));
-            return new JSString(j.value.Substring(start.IntValue, length.IntValue));
+                return new JSString(@this.Substring(start.IntValue));
+            return new JSString(@this.Substring(start.IntValue, length.IntValue));
         }
 
         [Prototype("substr")]
@@ -81,241 +79,195 @@ namespace WebAtoms.CoreJS.Core
         }
 
         [Prototype("charCodeAt")]
-        internal static JSValue CharCodeAt(JSValue t, params JSValue[] a)
+        internal static JSValue CharCodeAt(in Arguments a)
         {
-            var text = AsString(t);
+            var text = AsString(a.This);
             var at = a.TryGetAt(0, out var n) ? n.IntValue : 0;
             return new JSString(new string(text[at], 1));
         }
 
         [Prototype("codePointAt")]
-        internal static JSValue CodePointAt(JSValue t, params JSValue[] a)
+        internal static JSValue CodePointAt(in Arguments a)
         {
             return null;
         }
 
         [Prototype("concat")]
-        internal static JSValue Concat(JSValue t, params JSValue[] a)
+        internal static JSValue Concat(in Arguments a)
         {
-            var j = t as JSString;
-            if (j == null)
-                return JSUndefined.Value;
-            if (!a.TryGetAt(0, out var n))
-                return JSUndefined.Value;
-            var at = AsString(a[0]);
-            return j.AddValue(at);
+            var @this = a.This.AsString();
+            var f = a.Get1();
+            return new JSString( @this + f.ToString() );
         }
 
         [Prototype("endsWith")]
-        internal static JSValue EndsWith(JSValue t, params JSValue[] a)
+        internal static JSValue EndsWith(in Arguments a)
         {
-            var j = t as JSString;
-            var s = a[0] as JSString;
-            var n = a.TryGetAt(1, out var i) ? i.IntValue : -1;
-            if (j == null || s == null)
-                return JSUndefined.Value;
-            n = Math.Min(Math.Max(0, n),j.Length);
-            if (s.Length > n)
-                return JSBooleanPrototype.False;
-            var ar = new JSValue[2];
-            ar[0].AddValue(n-s.Length);
-            ar[1].AddValue(s.Length);
-            if (Substring(j, ar) == s)
-                return JSBooleanPrototype.True;
-            else
-                return JSBooleanPrototype.False;
+            var @this = a.This.AsString();
+            var f = a.Get1();
+            return @this.EndsWith(f.ToString()) ? JSBoolean.True : JSBoolean.False;
         }
 
-        [Prototype("startsWith")]
-        internal static JSValue StartsWith(JSValue t, params JSValue[] a)
-        {
-            var j = t as JSString;
-            var s = a[0] as JSString;
-            var n = a.TryGetAt(1, out var i) ? i.IntValue : -1;
-            if (j == null || s == null)
-                return JSUndefined.Value;
-            n = Math.Min(Math.Max(0, n), j.Length);
-            if (s.Length > n)
-                return JSBooleanPrototype.False;
-            var ar = new JSValue[2];
-            ar[0].AddValue(n);
-            ar[1].AddValue(s.Length);
-            if (Substring(j, ar) == s)
-                return JSBooleanPrototype.True;
-            else
-                return JSBooleanPrototype.False;
-        }
+        //[Prototype("startsWith")]
+        //internal static JSValue StartsWith(JSValue t, params JSValue[] a)
+        //{
+        //    var j = t as JSString;
+        //    var s = a[0] as JSString;
+        //    var n = a.TryGetAt(1, out var i) ? i.IntValue : -1;
+        //    if (j == null || s == null)
+        //        return JSUndefined.Value;
+        //    n = Math.Min(Math.Max(0, n), j.Length);
+        //    if (s.Length > n)
+        //        return JSBoolean.False;
+        //    var ar = new JSValue[2];
+        //    ar[0].AddValue(n);
+        //    ar[1].AddValue(s.Length);
+        //    if (Substring(j, ar) == s)
+        //        return JSBoolean.True;
+        //    else
+        //        return JSBoolean.False;
+        //}
 
-        [Prototype("includes")]
-        internal static JSValue Includes(JSValue t, params JSValue[] a)
-        {
-            var j = t as JSString;
-            var s = a[0] as JSString;
-            var n = a.TryGetAt(1, out var i) ? i.IntValue : -1;
-            if (j == null || s == null)
-                return JSUndefined.Value;
-            else
-                return ()
-        }
+        //[Prototype("includes")]
+        //internal static JSValue Includes(JSValue t, params JSValue[] a)
+        //{
+        //    var j = t as JSString;
+        //    var s = a[0] as JSString;
+        //    var n = a.TryGetAt(1, out var i) ? i.IntValue : -1;
+        //    if (j == null || s == null)
+        //        return JSUndefined.Value;
+        //    else
+        //        return ()
+        //}
 
         [Prototype("indexOf")]
-        internal static JSValue IndexOf(JSValue t, params JSValue[] a)
+        internal static JSValue IndexOf(in Arguments a)
         {
-            var j = t as JSArray;
-            var s = a[0] as JSArray;
-
-            if (j == null || s == null|| j.Length == 0 || s.Length == 0)
-                return JSUndefined.Value;
-            if (s.Length > j.Length)
-                return j.AddValue(-1);
-            if (!a.TryGetAt(1, out var index))
-            {
-                var count = new UInt32();
-                var f = false;
-                count = 0;
-                foreach(var k in j.AllElements)
-                {
-                    if(count < s.Length)
-                    foreach (var l in s.AllElements)
-                    {
-                        if (l == k)
-                        {
-                            f = true;
-                            count++;
-                        }
-                        else
-                        {
-                            f = false;
-                            count = 0;
-                            break;
-                        }
-                    }
-                    
-                }
-                return 
-            } else {
-                return
-            }
+            var @this = a.This.AsString();
+            var (text, start) = a.Get2();
+            var startIndex = start.IsUndefined ? 0 : start.IntValue;
+            var index = @this.IndexOf(text.ToString(), startIndex);
+            return new JSNumber(index);
         }
 
-        [Prototype("lastIndexOf")]
-        internal static JSValue LastIndexOF(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("lastIndexOf")]
+        //internal static JSValue LastIndexOF(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("match")]
-        internal static JSValue Match(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("match")]
+        //internal static JSValue Match(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("matchAll")]
-        internal static JSValue MatchAll(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("matchAll")]
+        //internal static JSValue MatchAll(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("normalize")]
-        internal static JSValue Normalize(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("normalize")]
+        //internal static JSValue Normalize(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("padEnd")]
-        internal static JSValue PadEnd(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("padEnd")]
+        //internal static JSValue PadEnd(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("padStart")]
-        internal static JSValue PadStart(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("padStart")]
+        //internal static JSValue PadStart(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("repeat")]
-        internal static JSValue Repeat(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("repeat")]
+        //internal static JSValue Repeat(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("replace")]
-        internal static JSValue Replace(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("replace")]
+        //internal static JSValue Replace(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("replaceAll")]
-        internal static JSValue ReplaceAll(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("replaceAll")]
+        //internal static JSValue ReplaceAll(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("search")]
-        internal static JSValue Search(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("search")]
+        //internal static JSValue Search(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("slice")]
-        internal static JSValue Slice(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("slice")]
+        //internal static JSValue Slice(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("split")]
-        internal static JSValue Split(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("split")]
+        //internal static JSValue Split(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("toLocaleLowerCase")]
-        internal static JSValue ToLocaleLowerCase(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("toLocaleLowerCase")]
+        //internal static JSValue ToLocaleLowerCase(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("toLocaleUpperCase")]
-        internal static JSValue ToLocaleUpperCase(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("toLocaleUpperCase")]
+        //internal static JSValue ToLocaleUpperCase(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("toLowerCase")]
-        internal static JSValue ToLowerCase(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("toLowerCase")]
+        //internal static JSValue ToLowerCase(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("toUpperCase")]
-        internal static JSValue ToUpperCase(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("toUpperCase")]
+        //internal static JSValue ToUpperCase(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("trim")]
-        internal static JSValue Trim(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("trim")]
+        //internal static JSValue Trim(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("trimEnd")]
-        internal static JSValue TrimEnd(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("trimEnd")]
+        //internal static JSValue TrimEnd(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("trimstart")]
-        internal static JSValue TrimStart(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("trimstart")]
+        //internal static JSValue TrimStart(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
 
-        [Prototype("valueOf")]
-        internal static JSValue ValueOf(JSValue t, params JSValue[] a)
-        {
-            return
-        }
+        //[Prototype("valueOf")]
+        //internal static JSValue ValueOf(JSValue t, params JSValue[] a)
+        //{
+        //    return
+        //}
     }
 }
