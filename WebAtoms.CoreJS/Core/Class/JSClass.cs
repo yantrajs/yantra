5a13f1b58dbd206162151d1c58abcca2af186646
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-10-09 09:07:51 +0530

    Added static

--- WebAtoms.CoreJS/Core/Class/JSClass.cs
+++ WebAtoms.CoreJS/Core/Class/JSClass.cs
@@ -55,14 +55,14 @@ namespace WebAtoms.CoreJS.Core
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal JSClass AddStaticProperty(KeyString name, JSFunction getter, JSFunction setter)
         {
-            this.prototype.ownProperties[name.Key] = JSProperty.Property(name, getter.f, setter?.f, JSPropertyAttributes.ConfigurableProperty);
+            this.ownProperties[name.Key] = JSProperty.Property(name, getter.f, setter?.f, JSPropertyAttributes.ConfigurableProperty);
             return this;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal JSClass AddStaticMethod(KeyString name, JSValue value)
         {
-            this.prototype.ownProperties[name.Key] = JSProperty.Property(name, value, JSPropertyAttributes.ConfigurableValue);
+            this.ownProperties[name.Key] = JSProperty.Property(name, value, JSPropertyAttributes.ConfigurableValue);
             return this;
         }
 
