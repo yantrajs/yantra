4a46387ce1cd96f52c3b3d353ca846239a4d18ce
simmi <simmi@neuro>
2020-10-22 16:22:50 +0530

    Int8Array implementation

---
+++ WebAtoms.CoreJS.Tests/Generator/Files/es5/Array/Typed/from.js
@@ -0,0 +1,9 @@
+﻿// let uint16 = new Int8Array;
+let uint16 = Int8Array.from('12345');
+// let output = [1, 2, 3, 4, 5];
+assert.strictEqual(1, uint16[0]);
+assert.strictEqual(2, uint16[1]);
+assert.strictEqual(3, uint16[2]);
+assert.strictEqual(4, uint16[3]);
+assert.strictEqual(5, uint16[4]);
+// assert.strictEqual("1,2,3,4,5", uint16.toString());
---
+++ WebAtoms.CoreJS/Core/Array/Typed/TypedArrayPrototype.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAtoms.CoreJS.Core.Typed
+{
+    class TypedArrayPrototype
+    {
+    }
+}
