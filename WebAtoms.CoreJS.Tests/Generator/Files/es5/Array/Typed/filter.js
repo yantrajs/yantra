9407fd2e87bf6a7fb8cc6d9c2cc91789d944c0f0
simmi <simmi@neuro>
2020-10-27 16:10:11 +0530

    few more tecs added to filter

--- WebAtoms.CoreJS.Tests/Generator/Files/es5/Array/Typed/filter.js
+++ WebAtoms.CoreJS.Tests/Generator/Files/es5/Array/Typed/filter.js
@@ -8,3 +8,18 @@ let negInt8 = int8.filter(isNegative);
 //console.log(negInt8);
 assert.strictEqual("-10,-30,-50", negInt8.toString());
 // expected output: Int8Array [-10, -30, -50]
+
+int8 = new Int8Array([1, 2, 3]);
+assert.throws(() => {
+    int8.filter(true);
+});
+// assert.strictEqual("TypeError", int8.filter(true));
+int8 = new Int8Array([1, 2, 3]);
+assert.throws(() => {
+    int8.filter(1);
+});
+
+int8 = new Int8Array([1, 2, 3]);
+assert.throws(() => {
+    int8.filter({});
+});
