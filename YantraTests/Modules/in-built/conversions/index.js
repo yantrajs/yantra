5235bc40b766e463d0dafa09dac2b32e6f8474d1
Akash Kava <ackava@gmail.com>
2020-10-28 14:01:49 +0530

    wip

--- YantraTests/Modules/in-built/conversions/index.js
+++ YantraTests/Modules/in-built/conversions/index.js
@@ -1,10 +1,10 @@
 ﻿var clr = require("clr").default;
 
-var IntArray = clr.getClass("System.Int32[], mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
+var ClrInt32Array = clr.getClass("System.Int32[], mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
 
-var SystemArray = clr.getClass("System.Array");
+var ClrArray = clr.getClass("System.Array");
 
-var a = new IntArray(10);
+var a = new ClrInt32Array(10);
 
 a[0] = 1;
 a[1] = 2;
@@ -12,12 +12,10 @@ a[1] = 2;
 assert.strictEqual(1, a[0]);
 assert.strictEqual(2, a[1]);
 
-//console.log(Object.keys(IntArray.prototype));
-//console.log(Object.keys(SystemArray.prototype));
-assert(a instanceof SystemArray);
+assert(a instanceof ClrArray);
+assert(a instanceof ClrInt32Array);
 // assert(IntArray.prototype.length);
 
-// length should be 10...
 assert.strictEqual(10, a.length);
 
 var List = clr.getClass("System.Collections.Generic.List`1, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
