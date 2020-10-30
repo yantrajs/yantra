7a876e179397d7f6b3d3dc21466a7740d50e9b6c
simmi <simmi@neuro>
2020-10-30 15:39:04 +0530

    reduceRight unit test implemented

--- WebAtoms.CoreJS.Tests/Generator/Files/es5/Array/Typed/reduceRight.js
+++ WebAtoms.CoreJS.Tests/Generator/Files/es5/Array/Typed/reduceRight.js
@@ -1,1 +1,41 @@
-﻿let arr = new Int8Array([1, 2, 3]);
+﻿
+
+let uint = new Int8Array([1, 2, 3]);
+function val(accum, value, index, array) {
+    return accum + value;
+}
+assert.strictEqual(6, uint.reduceRight(val));
+
+function val5(accum, value, index, array) {
+    return accum + index;
+}
+
+assert.strictEqual(4, uint.reduceRight(val5));
+
+function val2(accum, value, index, array) {
+    return accum + array[index];
+}
+
+assert.strictEqual(6, uint.reduceRight(val2));
+
+let indices = [];
+function val3(accum, value, index, array) {
+    return indices.push(index);
+}
+uint.reduceRight(val3)
+assert.strictEqual("1,0", indices.toString());
+
+
+assert.throws(() => {
+    uint8.reduceRight(true);
+});
+
+assert.throws(() => {
+    uint8.reduceRight(1);
+});
+
+
+assert.throws(() => {
+    uint8.reduceRight({});
+});
+
