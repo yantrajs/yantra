aacdabce7063c7ee85e6bae5ed6060920a5b24da
Akash Kava <ackava@gmail.com>
2020-12-11 16:04:25 +0530

    wip

--- YantraJS.Core/DynamicHelper.cs
+++ YantraJS.Core/DynamicHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace YantraJS.Core
@@ -8,9 +9,11 @@ namespace YantraJS.Core
     internal static class DynamicHelper
     {
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T CompileDynamic<T>(this Expression<T> exp)
         {
-            return Microsoft.Scripting.Generation.CompilerHelpers.Compile<T>(exp, true);
+            // return Microsoft.Scripting.Generation.CompilerHelpers.Compile<T>(exp, true);
+            return exp.Compile();
         }
 
     }
