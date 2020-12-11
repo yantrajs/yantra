685c38f2cefe18aac6730d57594215e32386fc41
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-12-11 21:24:56 +0530

    wip

--- YantraJS.Core/DynamicHelper.cs
+++ YantraJS.Core/DynamicHelper.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
 using System.Text;
 
@@ -13,7 +16,17 @@ namespace YantraJS.Core
         public static T CompileDynamic<T>(this Expression<T> exp)
         {
             // return Microsoft.Scripting.Generation.CompilerHelpers.Compile<T>(exp, true);
-            return exp.Compile();
+            var fx = exp.Compile();
+
+            // may be this way we can move out from
+            // here and move to different assembly...
+            //var d = (Delegate)(object)fx;
+            //var type = typeof(System.Reflection.Emit.DynamicMethod);
+            //var rtd = type.GetNestedType("RTDynamicMethod", System.Reflection.BindingFlags.NonPublic);
+            //var field = rtd.GetTypeInfo().DeclaredFields.FirstOrDefault(x => x.Name == "m_owner");
+            //DynamicMethod dm = field?.GetValue(d.Method) as DynamicMethod;
+            //var il = dm.GetILGenerator();
+            return fx;
         }
 
     }
