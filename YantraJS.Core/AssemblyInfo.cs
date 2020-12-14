d513ac46675c75ed8cd5f420442d731b3698e5c2
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-12-14 09:44:57 +0530

    Added runtime

--- YantraJS.Core/AssemblyInfo.cs
+++ YantraJS.Core/AssemblyInfo.cs
@@ -6,4 +6,7 @@ using System.Text;
 #if !WEB_ATOMS
 [assembly: InternalsVisibleTo("YantraJS.Tests")]
 [assembly: InternalsVisibleTo("YantraJS.Core.Tests")]
+
+// used by Dynamic Assembly to access internals
+[assembly: InternalsVisibleTo("YantraJS.Runtime")]
 #endif
