d0e0f09b1788bb51376b56c9b592d16de1c44331
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-01-20 11:36:10 +0530

    wip

--- YantraJS.Core.Tests/Generator/Files/es5/Objects/Reflect/Reflect.js
+++ YantraJS.Core.Tests/Generator/Files/es5/Objects/Reflect/Reflect.js
@@ -1132,6 +1132,248 @@ var Reflect;
 })(Reflect || (Reflect = {}));
 //# sourceMappingURL=Reflect.js.map
 
+//!function (o) {
+//    !function (t) {
+//        var e = "object" == typeof global ? global : "object" == typeof self ? self : "object" == typeof this ? this : Function("return this;")(), r = n(o); function n(r, n) { return function (t, e) { "function" != typeof r[t] && Object.defineProperty(r, t, { configurable: !0, writable: !0, value: e }), n && n(t, e); }; } void 0 === e.Reflect ? e.Reflect = o : r = n(e.Reflect, r), function (t) {
+//            var f = Object.prototype.hasOwnProperty, e = "function" == typeof Symbol, i = e && void 0 !== Symbol.toPrimitive ? Symbol.toPrimitive : "@@toPrimitive", s = e && void 0 !== Symbol.iterator ? Symbol.iterator : "@@iterator", r = "function" == typeof Object.create, n = { __proto__: [] } instanceof Array, o = !r && !n, c = { create: r ? function () { return S(Object.create(null)); } : n ? function () { return S({ __proto__: null }); } : function () { return S({}); }, has: o ? function (t, e) { return f.call(t, e); } : function (t, e) { return e in t; }, get: o ? function (t, e) { return f.call(t, e) ? t[e] : void 0; } : function (t, e) { return t[e]; } }, u = Object.getPrototypeOf(Function), a = "object" == typeof process && process.env && "true" === process.env.REFLECT_METADATA_USE_MAP_POLYFILL, h = a || "function" != typeof Map || "function" != typeof Map.prototype.entries ? function () {
+//                var o = {}, r = [], e = function () {
+//                    function t(t, e, r) { this._index = 0, this._keys = t, this._values = e, this._selector = r; } return t.prototype["@@iterator"] = function () { return this; }, t.prototype[s] = function () { return this; }, t.prototype.next = function () {
+//                        var t = this._index; if (0 <= t && t < this._keys.length) {
+//                            var e = this._selector(this._keys[t], this._values[t]);
+//                            return t + 1 >= this._keys.length ? (this._index = -1, this._keys = r, this._values = r) : this._index++, { value: e, done: !1 };
+//                        } return { value: void 0, done: !0 };
+//                    }, t.prototype.throw = function (t) { throw 0 <= this._index && (this._index = -1, this._keys = r, this._values = r), t; }, t.prototype.return = function (t) { return 0 <= this._index && (this._index = -1, this._keys = r, this._values = r), { value: t, done: !0 }; }, t;
+//                }(); return function () {
+//                    function t() { this._keys = [], this._values = [], this._cacheKey = o, this._cacheIndex = -2; } return Object.defineProperty(t.prototype, "size", { get: function () { return this._keys.length; }, enumerable: !0, configurable: !0 }), t.prototype.has = function (t) { return 0 <= this._find(t, !1); }, t.prototype.get = function (t) { var e = this._find(t, !1); return 0 <= e ? this._values[e] : void 0; }, t.prototype.set = function (t, e) { var r = this._find(t, !0); return this._values[r] = e, this; }, t.prototype.delete = function (t) {
+//                        var e = this._find(t, !1); if (0 <= e) {
+//                            for (var r = this._keys.length, n = e + 1; n < r; n++)
+//                                this._keys[n - 1] = this._keys[n], this._values[n - 1] = this._values[n];
+//                            return this._keys.length--, this._values.length--, t === this._cacheKey && (this._cacheKey = o, this._cacheIndex = -2), !0;
+//                        } return !1;
+//                    }, t.prototype.clear = function () { this._keys.length = 0, this._values.length = 0, this._cacheKey = o, this._cacheIndex = -2; }, t.prototype.keys = function () { return new e(this._keys, this._values, n); }, t.prototype.values = function () { return new e(this._keys, this._values, i); }, t.prototype.entries = function () { return new e(this._keys, this._values, u); }, t.prototype["@@iterator"] = function () { return this.entries(); }, t.prototype[s] = function () { return this.entries(); }, t.prototype._find = function (t, e) { return this._cacheKey !== t && (this._cacheIndex = this._keys.indexOf(this._cacheKey = t)), this._cacheIndex < 0 && e && (this._cacheIndex = this._keys.length, this._keys.push(t), this._values.push(void 0)), this._cacheIndex; }, t;
+//                }(); function n(t, e) { return t; } function i(t, e) { return e; } function u(t, e) { return [t, e]; }
+//            }() : Map, l = a || "function" != typeof Set || "function" != typeof Set.prototype.entries ? function () { function t() { this._map = new h; } return Object.defineProperty(t.prototype, "size", { get: function () { return this._map.size; }, enumerable: !0, configurable: !0 }), t.prototype.has = function (t) { return this._map.has(t); }, t.prototype.add = function (t) { return this._map.set(t, t), this; }, t.prototype.delete = function (t) { return this._map.delete(t); }, t.prototype.clear = function () { this._map.clear(); }, t.prototype.keys = function () { return this._map.keys(); }, t.prototype.values = function () { return this._map.values(); }, t.prototype.entries = function () { return this._map.entries(); }, t.prototype["@@iterator"] = function () { return this.keys(); }, t.prototype[s] = function () { return this.keys(); }, t; }() : Set, p = new (a || "function" != typeof WeakMap ? function () {
+//                var o = 16, e = c.create(), r = n(); return function () { function t() { this._key = n(); } return t.prototype.has = function (t) { var e = i(t, !1); return void 0 !== e && c.has(e, this._key); }, t.prototype.get = function (t) { var e = i(t, !1); return void 0 !== e ? c.get(e, this._key) : void 0; }, t.prototype.set = function (t, e) { var r = i(t, !0); return r[this._key] = e, this; }, t.prototype.delete = function (t) { var e = i(t, !1); return void 0 !== e && delete e[this._key]; }, t.prototype.clear = function () { this._key = n(); }, t; }(); function n() {
+//                    for (var t; t = "@@WeakMap@@" + a(), c.has(e, t);)
+//                        ; return e[t] = !0, t;
+//                } function i(t, e) {
+//                    if (!f.call(t, r)) {
+//                        if (!e)
+//                            return;
+//                        Object.defineProperty(t, r, { value: c.create() });
+//                    } return t[r];
+//                } function u(t, e) {
+//                    for (var r = 0; r < e; ++r)
+//                        t[r] = 255 * Math.random() | 0; return t;
+//                } function a() {
+//                    var t = function (t) {
+//                        if ("function" == typeof Uint8Array)
+//                            return "undefined" != typeof crypto ? crypto.getRandomValues(new Uint8Array(t)) : "undefined" != typeof msCrypto ? msCrypto.getRandomValues(new Uint8Array(t)) : u(new Uint8Array(t), t); return u(new Array(t), t);
+//                    }(o); t[6] = 79 & t[6] | 64, t[8] = 191 & t[8] | 128; for (var e = "", r = 0; r < o; ++r) {
+//                        var n = t[r];
+//                        4 !== r && 6 !== r && 8 !== r || (e += "-"), n < 16 && (e += "0"), e += n.toString(16).toLowerCase();
+//                    } return e;
+//                }
+//            }() : WeakMap); function y(t, e, r) {
+//                var n = p.get(t); if (b(n)) {
+//                    if (!r)
+//                        return;
+//                    n = new h, p.set(t, n);
+//                } var o = n.get(e); if (b(o)) {
+//                    if (!r)
+//                        return;
+//                    o = new h, n.set(e, o);
+//                } return o;
+//            } function v(t, e, r) { var n = y(e, r, !1); return !b(n) && !!n.has(t); } function _(t, e, r) {
+//                var n = y(e, r, !1); if (!b(n))
+//                    return n.get(t);
+//            } function d(t, e, r, n) { var o = y(r, n, !0); o.set(t, e); } function w(t, e) {
+//                var r = [], n = y(t, e, !1); if (b(n))
+//                    return r; for (var o, i = n.keys(), u = function (t) {
+//                        var e = M(t, s); if (!j(e))
+//                            throw new TypeError; var r = e.call(t); if (!m(r))
+//                            throw new TypeError; return r;
+//                    }(i), a = 0; ;) {
+//                    var f = (void 0, !(o = u.next()).done && o);
+//                    if (!f)
+//                        return r.length = a, r;
+//                    var c = f.value;
+//                    try {
+//                        r[a] = c;
+//                    }
+//                    catch (t) {
+//                        try {
+//                            A(u);
+//                        }
+//                        finally {
+//                            throw t;
+//                        }
+//                    }
+//                    a++;
+//                }
+//            } function g(t) {
+//                if (null === t)
+//                    return 1; switch (typeof t) {
+//                        case "undefined": return 0;
+//                        case "boolean": return 2;
+//                        case "string": return 3;
+//                        case "symbol": return 4;
+//                        case "number": return 5;
+//                        case "object": return null === t ? 1 : 6;
+//                        default: return 6;
+//                    }
+//            } function b(t) { return void 0 === t; } function k(t) { return null === t; } function m(t) { return "object" == typeof t ? null !== t : "function" == typeof t; } function E(t, e) {
+//                switch (g(t)) {
+//                    case 0:
+//                    case 1:
+//                    case 2:
+//                    case 3:
+//                    case 4:
+//                    case 5: return t;
+//                } var r = 3 === e ? "string" : 5 === e ? "number" : "default", n = M(t, i); if (void 0 !== n) {
+//                    var o = n.call(t, r);
+//                    if (m(o))
+//                        throw new TypeError;
+//                    return o;
+//                } return function (t, e) {
+//                    if ("string" === e) {
+//                        var r = t.toString;
+//                        if (j(r)) {
+//                            var n = r.call(t);
+//                            if (!m(n))
+//                                return n;
+//                        }
+//                        var o = t.valueOf;
+//                        if (j(o)) {
+//                            var n = o.call(t);
+//                            if (!m(n))
+//                                return n;
+//                        }
+//                    }
+//                    else {
+//                        var o = t.valueOf;
+//                        if (j(o)) {
+//                            var n = o.call(t);
+//                            if (!m(n))
+//                                return n;
+//                        }
+//                        var i = t.toString;
+//                        if (j(i)) {
+//                            var n = i.call(t);
+//                            if (!m(n))
+//                                return n;
+//                        }
+//                    } throw new TypeError;
+//                }(t, "default" === r ? "number" : r);
+//            } function T(t) { var e = E(t, 3); return "symbol" == typeof e ? e : "" + e; } function O(t) { return Array.isArray ? Array.isArray(t) : t instanceof Object ? t instanceof Array : "[object Array]" === Object.prototype.toString.call(t); } function j(t) { return "function" == typeof t; } function x(t) { return "function" == typeof t; } function M(t, e) {
+//                var r = t[e]; if (null != r) {
+//                    if (!j(r))
+//                        throw new TypeError;
+//                    return r;
+//                }
+//            } function A(t) { var e = t.return; e && e.call(t); } function P(t) {
+//                var e = Object.getPrototypeOf(t); if ("function" != typeof t || t === u)
+//                    return e; if (e !== u)
+//                    return e; var r = t.prototype, n = r && Object.getPrototypeOf(r); if (null == n || n === Object.prototype)
+//                    return e; var o = n.constructor; return "function" != typeof o ? e : o === t ? e : o;
+//            } function S(t) { return t.__ = void 0, delete t.__, t; } t("decorate", function (t, e, r, n) {
+//                {
+//                    if (b(r)) {
+//                        if (!O(t))
+//                            throw new TypeError;
+//                        if (!x(e))
+//                            throw new TypeError;
+//                        return function (t, e) {
+//                            for (var r = t.length - 1; 0 <= r; --r) {
+//                                var n = t[r], o = n(e);
+//                                if (!b(o) && !k(o)) {
+//                                    if (!x(o))
+//                                        throw new TypeError;
+//                                    e = o;
+//                                }
+//                            } return e;
+//                        }(t, e);
+//                    }
+//                    if (!O(t))
+//                        throw new TypeError;
+//                    if (!m(e))
+//                        throw new TypeError;
+//                    if (!m(n) && !b(n) && !k(n))
+//                        throw new TypeError;
+//                    return k(n) && (n = void 0), r = T(r), function (t, e, r, n) {
+//                        for (var o = t.length - 1; 0 <= o; --o) {
+//                            var i = t[o], u = i(e, r, n);
+//                            if (!b(u) && !k(u)) {
+//                                if (!m(u))
+//                                    throw new TypeError;
+//                                n = u;
+//                            }
+//                        } return n;
+//                    }(t, e, r, n);
+//                }
+//            }), t("metadata", function (r, n) {
+//                return function (t, e) {
+//                    if (!m(t))
+//                        throw new TypeError; if (!b(e) && !function (t) {
+//                            switch (g(t)) {
+//                                case 3:
+//                                case 4: return !0;
+//                                default: return !1;
+//                            }
+//                        }(e))
+//                        throw new TypeError; d(r, n, t, e);
+//                };
+//            }), t("defineMetadata", function (t, e, r, n) {
+//                if (!m(r))
+//                    throw new TypeError; b(n) || (n = T(n)); return d(t, e, r, n);
+//            }), t("hasMetadata", function (t, e, r) {
+//                if (!m(e))
+//                    throw new TypeError; b(r) || (r = T(r)); return function t(e, r, n) {
+//                        var o = v(e, r, n); if (o)
+//                            return !0; var i = P(r); if (!k(i))
+//                            return t(e, i, n); return !1;
+//                    }(t, e, r);
+//            }), t("hasOwnMetadata", function (t, e, r) {
+//                if (!m(e))
+//                    throw new TypeError; b(r) || (r = T(r)); return v(t, e, r);
+//            }), t("getMetadata", function (t, e, r) {
+//                if (!m(e))
+//                    throw new TypeError; b(r) || (r = T(r)); return function t(e, r, n) {
+//                        var o = v(e, r, n); if (o)
+//                            return _(e, r, n); var i = P(r); if (!k(i))
+//                            return t(e, i, n); return;
+//                    }(t, e, r);
+//            }), t("getOwnMetadata", function (t, e, r) {
+//                if (!m(e))
+//                    throw new TypeError; b(r) || (r = T(r)); return _(t, e, r);
+//            }), t("getMetadataKeys", function (t, e) {
+//                if (!m(t))
+//                    throw new TypeError; b(e) || (e = T(e)); return function t(e, r) {
+//                        var n = w(e, r); var o = P(e); if (null === o)
+//                            return n; var i = t(o, r); if (i.length <= 0)
+//                            return n; if (n.length <= 0)
+//                            return i; var u = new l; var a = []; for (var f = 0, c = n; f < c.length; f++) {
+//                                var s = c[f], h = u.has(s);
+//                                h || (u.add(s), a.push(s));
+//                            } for (var p = 0, y = i; p < y.length; p++) {
+//                                var s = y[p], h = u.has(s);
+//                                h || (u.add(s), a.push(s));
+//                            } return a;
+//                    }(t, e);
+//            }), t("getOwnMetadataKeys", function (t, e) {
+//                if (!m(t))
+//                    throw new TypeError; b(e) || (e = T(e)); return w(t, e);
+//            }), t("deleteMetadata", function (t, e, r) {
+//                if (!m(e))
+//                    throw new TypeError; b(r) || (r = T(r)); var n = y(e, r, !1); if (b(n))
+//                    return !1; if (!n.delete(t))
+//                    return !1; if (0 < n.size)
+//                    return !0; var o = p.get(e); return o.delete(r), 0 < o.size || p.delete(e), !0;
+//            });
+//        }(r);
+//    }();
+//}(Reflect || (Reflect = {}));
+
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
@@ -1147,15 +1389,26 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 
 var r1 = {};
+
+function ip(t, n, i) {
+    console.log(`${t} ${n} ${i}`);
+}
+
 function b(t, n, i) {
     r1.a = Reflect.getMetadata("design:paramtypes", t, n);
 }
 
 var mm = (function () {
 
-    let AtomComponent = class AtomComponent { };
+    let AtomComponent = class AtomComponent {
+    };
     var p = __param(0, b);
     var m = __metadata("design:paramtypes", [String]);
+
+    __decorate([
+        ip,
+        __metadata("design:type", Object)
+    ], AtomComponent.prototype, "data", void 0);
 
     AtomComponent = __decorate([
         p,
