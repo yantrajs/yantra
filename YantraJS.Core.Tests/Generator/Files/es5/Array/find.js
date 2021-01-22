d90f0ed5d68d56f5d7e8f377a5b8081e67dc62c1
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-01-22 21:46:24 +0530

    wip

--- YantraJS.Core.Tests/Generator/Files/es5/Array/find.js
+++ YantraJS.Core.Tests/Generator/Files/es5/Array/find.js
@@ -1,3 +1,37 @@
 ﻿const a = ["a", "b", "c", 2, 3];
 const first = a.find((x) => typeof x === "number");
 assert.strictEqual(2, first);
+
+class A { }
+
+class B { }
+
+let aa = [{ id: A, value: 1 }, { id: B, value: 2 }];
+assert.strictEqual(aa.find((x) => x.id === A).value, 1);
+assert.strictEqual(aa.find((x) => x.id === B).value, 2);
+
+class ArrayHelper {
+    static remove(a, filter) {
+        for (let i = 0; i < a.length; i++) {
+            const item = a[i];
+            if (filter(item)) {
+                a.splice(i, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+ArrayHelper.remove(aa, (x) => x.id === ArrayHelper || x.value === ArrayHelper);
+assert.strictEqual(2, aa.length);
+
+aa = [{ id: A, value: 1 }, { id: B, value: 2 }];
+ArrayHelper.remove(aa, (x) => x.id === A || x.value === A);
+assert.strictEqual(1, aa.length);
+assert.strictEqual(B, aa[0].id);
+
+aa = [{ id: A, value: 1 }, { id: B, value: 2 }];
+ArrayHelper.remove(aa, (x) => x.id === B || x.value === B);
+assert.strictEqual(1, aa.length);
+assert.strictEqual(A, aa[0].id);
---
+++ YantraJS.Core.Tests/Generator/Files/es6/Syntax/class/operator.js
@@ -0,0 +1,16 @@
+﻿class A {
+
+    constructor() {
+        this.ids = 1;
+    }
+
+    process() {
+        return this.ids++;
+    }
+}
+
+var a1 = new A();
+
+assert.strictEqual(a1.process(), 1);
+assert.strictEqual(a1.process(), 2);
+assert.strictEqual(a1.ids, 3);
