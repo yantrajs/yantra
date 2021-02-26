f626ecd0bbf54c315cfb6b1fe64e31992700f7af
Akash Kava <ackava@gmail.com>
2021-02-26 15:44:54 +0530

    Nested lambda fix remaining

--- YantraJS.Core/DynamicHelper.cs
+++ YantraJS.Core/DynamicHelper.cs
@@ -17,7 +17,13 @@ namespace YantraJS.Core
         {
             // return Microsoft.Scripting.Generation.CompilerHelpers.Compile<T>(exp, true);
             var fx = exp.Compile();
+
             return fx;
+
+            //// this will only work without nested lambda
+            //// nested lambda needs to be extracted and put into external
+            //// array of closures or static methods
+
             //// may be this way we can move out from
             //// here and move to different assembly...
             //var d = (Delegate)(object)fx;
@@ -27,14 +33,14 @@ namespace YantraJS.Core
             //DynamicMethod dm = field?.GetValue(d.Method) as DynamicMethod;
             //var il = dm.GetILGenerator();
 
-            //var newMethod = new DynamicMethod(exp.Name ?? "tail_call", 
-            //    dm.ReturnType, 
+            //var newMethod = new DynamicMethod(exp.Name ?? "tail_call",
+            //    dm.ReturnType,
             //    exp.Parameters.Select(x => x.IsByRef ? (x.Type.IsByRef ? x.Type : x.Type.MakeByRefType()) : x.Type).ToArray(), true);
             //var newIL = newMethod.GetILGenerator();
 
             //newIL.Emit(OpCodes.Ldnull);
             //newIL.Emit(OpCodes.Ldarg_0);
-            //// newIL.Emit(OpCodes.Tailcall, dm);
+            //newIL.Emit(OpCodes.Tailcall);
             //newIL.Emit(OpCodes.Call, dm);
             //newIL.Emit(OpCodes.Ret);
             //var newDelegate = newMethod.CreateDelegate(typeof(T));
