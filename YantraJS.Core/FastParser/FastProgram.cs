941e3322b1871d06e74d1231159496c23c1297a2
Akash Kava <ackava@gmail.com>
2021-03-02 17:39:03 +0530

    wip

---
+++ YantraJS.Core/FastParser/FastProgram.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YantraJS.Core.FastParser
+{
+    public class FastScanner
+    {
+
+    }
+
+    public enum FastNodeType
+    {
+
+    }
+
+    public class FastNode
+    {
+
+    }
+
+    public class FastExpression
+    {
+
+    }
+
+    public class FastBinaryExpression
+    {
+
+    }
+
+    public class FastStatement
+    {
+
+    }
+
+    public class FastBlock
+    {
+        public List<FastStatement> Statements;
+    }
+
+    public class FastProgram
+    {
+
+
+
+    }
+}
