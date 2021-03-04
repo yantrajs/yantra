266580229c4f5a2e2aec336cb0206e562454cff3
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-03-04 11:44:50 +0530

    wip

--- YantraJS.Core/FastParser/FastProgram.cs
+++ YantraJS.Core/FastParser/FastProgram.cs
@@ -1,21 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace YantraJS.Core.FastParser
 {
     public class FastScanner
     {
+        public readonly StringSpan Text;
 
+        public FastScanner(in StringSpan text)
+        {
+            this.Text = text;
+        }
     }
+
+    public delegate bool ParseToken(FastScanner scanner, out FastToken token);
 
     public readonly struct FastTokenType
     {
-
         public readonly int Id;
+        public readonly ParseToken Parser;
 
-        
+        private static int nextId = 0;
 
+        public FastTokenType(ParseToken parser)
+        {
+            this.Id = Interlocked.Increment(ref nextId);
+            this.Parser = parser;
+        }
+
+        public static bool operator == (in FastTokenType left, in FastTokenType right)
+        {
+            return left.Id == right.Id;
+        }
+        public static bool operator !=(in FastTokenType left, in FastTokenType right)
+        {
+            return left.Id != right.Id;
+        }
+
+        public static implicit operator FastTokenType(ParseToken parser)
+        {
+            return new FastTokenType(parser);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is  FastTokenType ft ? Id == ft.Id : false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+
+    }
+
+    public static class TokenTypes
+    {
+        // public static FastTokenType Identifier = (ParseToken) (scanner, out bool success) => { };
     }
 
     public readonly struct FastToken
