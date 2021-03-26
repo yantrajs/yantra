ff13c2e76e7dee886cd0762e3149554af4f58862
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-03-26 12:44:02 +0530

    wip

---
+++ YantraJS.Core/FastParser/Ast/AstForOfStatement.cs
@@ -0,0 +1,17 @@
+﻿namespace YantraJS.Core.FastParser
+{
+    public class AstForOfStatement : AstStatement
+    {
+        public readonly AstNode Init;
+        public readonly AstExpression Target;
+        public readonly AstStatement Body;
+
+        public AstForOfStatement(FastToken token, FastToken previousToken, AstNode beginNode, AstExpression target, AstStatement statement)
+            : base(token, FastNodeType.ForOfStatement, previousToken)
+        {
+            this.Init = beginNode;
+            this.Target = target;
+            this.Body = statement;
+        }
+    }
+}
--- YantraJS.Core/FastParser/Ast/AstForStatement.cs
+++ YantraJS.Core/FastParser/Ast/AstForStatement.cs
@@ -16,34 +16,4 @@
             this.Body = statement;
         }
     }
-
-    public class AstForInStatement : AstStatement
-    {
-        public readonly AstNode Init;
-        public readonly AstExpression Target;
-        public readonly AstStatement Body;
-
-        public AstForInStatement(FastToken token, FastToken previousToken, AstNode beginNode, AstExpression target, AstStatement statement)
-            : base(token, FastNodeType.ForInStatement, previousToken)
-        {
-            this.Init = beginNode;
-            this.Target = target;
-            this.Body = statement;
-        }
-    }
-
-    public class AstForOfStatement : AstStatement
-    {
-        public readonly AstNode Init;
-        public readonly AstExpression Target;
-        public readonly AstStatement Body;
-
-        public AstForOfStatement(FastToken token, FastToken previousToken, AstNode beginNode, AstExpression target, AstStatement statement)
-            : base(token, FastNodeType.ForOfStatement, previousToken)
-        {
-            this.Init = beginNode;
-            this.Target = target;
-            this.Body = statement;
-        }
-    }
 }
