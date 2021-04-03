b2f40257b9f5a64a3f5ac8a895494f786cbb304d
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-04-03 11:29:00 +0530

    wip

---
+++ YantraJS.Core/FastParser/Parser/FastParser.NextExpression.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YantraJS.Core.FastParser
+{
+
+    partial class FastParser
+    {
+
+        /// <summary>
+        /// NextExpression evaluates and reads next set of tokens,
+        /// It decides precedence of right side expression and combines
+        /// expressions and returns true/false if expression has parsed successfully.
+        /// 
+        /// It will return true if expression ends successfully.
+        /// 
+        /// It will return true if no computable expression is found. It will only
+        /// return false if parser does not find a valid expression, but it could be
+        /// parsed by some other rule.
+        /// 
+        /// It will rewrite next token as semicolon.
+        /// 
+        /// This uses recursive calls as using inbuilt .net stack is much easier rather
+        /// than using our own stack.
+        /// 
+        /// There might be some issue with stack overflow.. we will revisit it when needed.
+        /// In that case we might introduce local stack.
+        /// </summary>
+        /// <param name="previous"></param>
+        /// <param name="previousType"></param>
+        /// <param name="node"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        bool NextExpression(
+            ref AstExpression previous, ref TokenTypes previousType,
+            out AstExpression node, out TokenTypes type)
+        {
+
+            AstExpression right;
+            TokenTypes rightType;
+
+            switch(previousType)
+            {
+
+                /**
+                 * Following are single expression terminators
+                 */
+                
+                case TokenTypes.Comma:
+                case TokenTypes.LineTerminator:
+                case TokenTypes.SemiColon:
+                case TokenTypes.SquareBracketEnd:
+                case TokenTypes.BracketEnd:
+                case TokenTypes.CurlyBracketEnd:
+                case TokenTypes.Colon:
+                case TokenTypes.EOF:
+                    node = null;
+                    type = TokenTypes.SemiColon;
+                    return true;
+
+                // Associate right...
+                case TokenTypes.Assign:
+                case TokenTypes.AssignAdd:
+                case TokenTypes.AssignBitwideAnd:
+                case TokenTypes.AssignBitwideOr:
+                case TokenTypes.AssignDivide:
+                case TokenTypes.AssignLeftShift:
+                case TokenTypes.AssignMod:
+                case TokenTypes.AssignMultiply:
+                case TokenTypes.AssignPower:
+                case TokenTypes.AssignRightShift:
+                case TokenTypes.AssignSubtract:
+                case TokenTypes.AssignUnsignedRightShift:
+                case TokenTypes.AssignXor:
+                    stream.Consume();
+                    if (!Expression(out right))
+                        throw stream.Unexpected();
+                    previous = previous.Combine(previousType, right);
+                    node = null;
+                    type = TokenTypes.SemiColon;
+                    return true;
+
+                case TokenTypes.QuestionMark:
+                    stream.CheckAndConsume(previousType);
+                    if (!Expression(out var @true))
+                        throw stream.Unexpected();
+                    stream.Expect(TokenTypes.Colon);
+                    if (!Expression(out var @false))
+                        throw stream.Unexpected();
+                    previous = new AstConditionalExpression(previous, @true, @false);
+                    previousType = stream.Current.Type;
+                    return NextExpression(ref previous, ref previousType, out node, out type);
+            }
+
+            stream.CheckAndConsume(previousType);
+
+            if (!SingleMemberExpression(out node))
+            {
+                if (EndOfStatement())
+                {
+                    type = TokenTypes.SemiColon;
+                    return true;
+                }
+                type = TokenTypes.None;
+                return false;
+            }
+
+            var begin = Location;
+            type = begin.Token.Type;
+
+            switch (type)
+            {
+
+                case TokenTypes.Comma:
+                case TokenTypes.LineTerminator:
+                case TokenTypes.SemiColon:
+                case TokenTypes.SquareBracketEnd:
+                case TokenTypes.BracketEnd:
+                case TokenTypes.CurlyBracketEnd:
+                case TokenTypes.Colon:
+                case TokenTypes.EOF:
+                    // previous = previous.Combine(previousType, node);
+                    // node = null;
+                    type = TokenTypes.SemiColon;
+                    return true;
+
+
+                // associate right...
+                case TokenTypes.Assign:
+                case TokenTypes.AssignAdd:
+                case TokenTypes.AssignBitwideAnd:
+                case TokenTypes.AssignBitwideOr:
+                case TokenTypes.AssignDivide:
+                case TokenTypes.AssignLeftShift:
+                case TokenTypes.AssignMod:
+                case TokenTypes.AssignMultiply:
+                case TokenTypes.AssignPower:
+                case TokenTypes.AssignRightShift:
+                case TokenTypes.AssignSubtract:
+                case TokenTypes.AssignUnsignedRightShift:
+                case TokenTypes.AssignXor:
+                    throw new FastParseException(begin.Token, "Invalid left hand side assignemnt");
+
+                case TokenTypes.Multiply:
+                case TokenTypes.Divide:
+                case TokenTypes.Plus:
+                case TokenTypes.Minus:
+                case TokenTypes.BitwiseAnd:
+                case TokenTypes.BitwiseNot:
+                case TokenTypes.BooleanAnd:
+                case TokenTypes.BooleanOr:
+                case TokenTypes.Xor:
+                case TokenTypes.LeftShift:
+                case TokenTypes.RightShift:
+                case TokenTypes.Less:
+                case TokenTypes.LessOrEqual:
+                case TokenTypes.Greater:
+                case TokenTypes.GreaterOrEqual:
+                    stream.Consume();
+                    if (Precedes(type, previousType)) {
+                        if (!NextExpression(ref node, ref type, out right, out rightType))
+                            return true;
+                        if (type == TokenTypes.SemiColon)
+                            return true;
+                        node = node.Combine(type, right);
+                        type = rightType;
+                        return true;
+                    }
+                    previous = previous.Combine(previousType, node);
+                    previousType = type;
+                    return NextExpression(ref previous, ref previousType, out node, out type);
+                default:
+                    throw stream.Unexpected();
+            }
+        }
+
+        bool Precedes(TokenTypes left, TokenTypes right)
+        {
+            if (left != TokenTypes.SemiColon && left != TokenTypes.EOF) {
+                return left < right;
+            }
+            return false;
+        }
+
+        
+    }
+
+}
