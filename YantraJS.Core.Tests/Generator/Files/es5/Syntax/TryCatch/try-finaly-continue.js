a95430cc425f621ae2356c948048a64ad8dde301
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-04-22 22:03:39 +0530

    wip

---
+++ YantraJS.Core.Tests/Generator/Files/es5/Syntax/TryCatch/try-finaly-continue.js
@@ -0,0 +1,12 @@
+﻿var a = [1,2,3];
+var n = [];
+for (let i of a) {
+    try {
+        (undefined).name();
+    } finally {
+        n.push(i);
+        // continue;
+    }
+}
+assert.strictEqual(n.toString(), "1,2,3");
+console.log("This test is pending !!")
