35689d814e4bcfcc452a5c37388e0bc1512d27f6
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-04-22 22:31:20 +0530

    finally control issue added in unsupported

--- YantraJS.Core.Tests/Generator/Files/es5/Syntax/TryCatch/try-finaly-continue.js
+++ YantraJS.Core.Tests/Generator/Files/es5/Syntax/TryCatch/try-finaly-continue.js
@@ -3,10 +3,12 @@ var n = [];
 for (let i of a) {
     try {
         (undefined).name();
-    } finally {
+    } catch (e) {
         n.push(i);
+    } finally {
         // continue;
     }
 }
+console.log(n.toString());
 assert.strictEqual(n.toString(), "1,2,3");
 console.log("This test is pending !!")
