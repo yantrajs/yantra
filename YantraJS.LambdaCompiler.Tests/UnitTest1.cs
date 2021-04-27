ec06a85044661686821eb649aa357ce3ca2f237c
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-04-27 11:00:59 +0530

    wip

--- YantraJS.LambdaCompiler.Tests/UnitTest1.cs
+++ YantraJS.LambdaCompiler.Tests/UnitTest1.cs
@@ -62,6 +62,42 @@ namespace YantraJS.LambdaCompiler.Tests
             Assert.AreEqual(1, fx(1, 2));
         }
 
+        [TestMethod]
+        public void SaveLocal()
+        {
+            var p1 = YExpression.Parameter(typeof(int));
+            var p2 = YExpression.Parameter(typeof(int));
+
+            var temp = YExpression.Parameter(typeof(int));
+
+            var @break = YExpression.Label();
+            var @continue = YExpression.Label();
+
+            var zero = YExpression.Constant(0);
+            var one = YExpression.Constant(1);
+
+            var loop = YExpression.Block(null, YExpression.Conditional(
+                    YExpression.Binary(p2, YOperator.LessOrEqual, zero),
+                    YExpression.GoTo(@break), null
+                    ),
+                YExpression.Assign(temp, YExpression.Binary(temp, YOperator.Add, p2)),
+                YExpression.Assign(p2, YExpression.Binary(p2, YOperator.Subtract, one)));
+
+            var body = YExpression.Block(new YParameterExpression[] { temp },
+                YExpression.Assign(temp, zero),
+                YExpression.Loop(loop, @break, @continue),
+                temp) ;
+
+            var lambda = YExpression.Lambda("mul",
+                body, new YParameterExpression[] { p1, p2 }
+                );
+
+            var fx = lambda.Compile<Func<int, int, int>>();
+
+            var n = fx(1, 2);
+            Assert.AreEqual(2, n);
+        }
+
 
         public T Compile<T>(YLambdaExpression exp)
         {
