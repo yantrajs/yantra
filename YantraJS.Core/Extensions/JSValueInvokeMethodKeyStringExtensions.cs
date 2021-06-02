64a08a0f20c9c709ca3cc4a18671c9a2a3059073
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-06-02 20:18:47 +0530

    Indexer replaced

--- YantraJS.Core/Extensions/JSValueInvokeMethodKeyStringExtensions.cs
+++ YantraJS.Core/Extensions/JSValueInvokeMethodKeyStringExtensions.cs
@@ -12,77 +12,70 @@ namespace YantraJS.Core
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static JSValue InvokeMethod(this JSValue @this, in KeyString name)
         {
-            var fx = @this[name];
-            if (fx.IsUndefined)
-                throw JSContext.Current.NewTypeError($"Method {name} not found on {@this}");
+            var fx = @this.GetMethod(name)
+                ?? throw JSContext.Current.NewTypeError($"Method {name} not found in {@this}");
             var a = new Arguments(@this);
-            return fx.InvokeFunction(a);
+            return fx(a);
         }
 
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static JSValue InvokeMethod(this JSValue @this, in KeyString name, JSValue arg0)
         {
-            var fx = @this[name];
-            if (fx.IsUndefined)
-                throw JSContext.Current.NewTypeError($"Method {name} not found on {@this}");
+            var fx = @this.GetMethod(name)
+                ?? throw JSContext.Current.NewTypeError($"Method {name} not found in {@this}");
             var a = new Arguments(@this, arg0);
-            return fx.InvokeFunction(a);
+            return fx(a);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static JSValue InvokeMethod(this JSValue @this, in KeyString name, JSValue arg0, JSValue arg1)
         {
-            var fx = @this[name];
-            if (fx.IsUndefined)
-                throw JSContext.Current.NewTypeError($"Method {name} not found on {@this}");
+            var fx = @this.GetMethod(name)
+                ?? throw JSContext.Current.NewTypeError($"Method {name} not found in {@this}");
             var a = new Arguments(@this, arg0, arg1);
-            return fx.InvokeFunction(a);
+            return fx(a);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static JSValue InvokeMethod(this JSValue @this, in KeyString name, JSValue arg0, JSValue arg1, JSValue arg2)
         {
-            var fx = @this[name];
-            if (fx.IsUndefined)
-                throw JSContext.Current.NewTypeError($"Method {name} not found on {@this}");
+            var fx = @this.GetMethod(name) 
+                ?? throw JSContext.Current.NewTypeError($"Method {name} not found in {@this}");
             var a = new Arguments(@this, arg0, arg1, arg2);
-            return fx.InvokeFunction(a);
+            return fx(a);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static JSValue InvokeMethod(this JSValue @this, in KeyString name, JSValue arg0, JSValue arg1, JSValue arg2, JSValue arg3)
         {
-            var fx = @this[name];
-            if (fx.IsUndefined)
-                throw JSContext.Current.NewTypeError($"Method {name} not found on {@this}");
+            var fx = @this.GetMethod(name)
+                ?? throw JSContext.Current.NewTypeError($"Method {name} not found in {@this}");
             var a = new Arguments(@this, arg0, arg1, arg2, arg3);
-            return fx.InvokeFunction(a);
+            return fx(a);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static JSValue InvokeMethod(this JSValue @this, in KeyString name, JSValue[] args)
         {
-            var fx = @this[name];
-            if (fx.IsUndefined)
-                throw JSContext.Current.NewTypeError($"Method {name} not found on {@this}");
+            var fx = @this.GetMethod(name)
+                ?? throw JSContext.Current.NewTypeError($"Method {name} not found in {@this}");
             var a = new Arguments(@this, args);
-            return fx.InvokeFunction(a);
+            return fx(a);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static JSValue InvokeMethodSpread(this JSValue @this, in KeyString name, JSValue[] args)
         {
-            var fx = @this[name];
-            if (fx.IsUndefined)
-                throw JSContext.Current.NewTypeError($"Method {name} not found on {@this}");
+            var fx = @this.GetMethod(name)
+                ?? throw JSContext.Current.NewTypeError($"Method {name} not found in {@this}");
             var length = 0;
             foreach ( var item in args)
             {
                 length += item.IsSpread ? item.Length : 1;
             }
             var a = new Arguments(@this, args, length);
-            return fx.InvokeFunction(a);
+            return fx(a);
         }
 
     }
