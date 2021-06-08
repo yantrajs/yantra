30157b1d7d914f2bd6083bb5a8e4cd2ffa07688b
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-06-08 21:07:14 +0530

    Adding test runner

---
+++ Test262Runner/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace Test262Runner
+{
+    class Program
+    {
+        static DirectoryInfo harnessFolder;
+
+        static DirectoryInfo language;
+
+        static DirectoryInfo builtins;
+
+        static DirectoryInfo root;
+
+        static DirectoryInfo output;
+
+        static string executable;
+
+        static void Main(string[] args)
+        {
+            // find all tests...
+            var pwd = new DirectoryInfo(Environment.CurrentDirectory);
+
+            root = pwd.Parent.Parent.Parent.Parent;
+
+            var test262 = Path.Combine(root.FullName, "test262");
+
+            harnessFolder = new DirectoryInfo(Path.Combine(test262, "harness"));
+
+            builtins = new DirectoryInfo(Path.Combine(test262, "test", "built-ins"));
+
+            language = new DirectoryInfo(Path.Combine(test262, "test", "language"));
+
+            if (args.Length == 0)
+            {
+                executable = Process.GetCurrentProcess().StartInfo.FileName;
+
+                RunTests(language);
+
+                return;
+            }
+        }
+
+        private static void RunTests(DirectoryInfo language)
+        {
+            foreach(var file in language.EnumerateFileSystemInfos())
+            {
+                if(file is DirectoryInfo di)
+                {
+                    RunTests(di);
+                    return;
+                }
+
+                RunTest(file as FileInfo);
+            }
+        }
+
+        private static void RunTest(FileInfo fileInfo)
+        {
+            var process = Process.Start(executable, fileInfo.FullName);
+            process.WaitForExit(60 * 60 * 2);
+        }
+    }
+}
