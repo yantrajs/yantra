33697386e50fd80d84a759e044fdfd5b6a1edf47
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-07-24 17:45:14 +0530

    Added factory

---
+++ WebAtoms.YantraJS/JSContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using YantraJS.Core;
+
+namespace WebAtoms.XF
+{
+    public class JSContextFactory : IJSContextFactory
+    {
+        public IJSContext Create()
+        {
+            // DictionaryCodeCache.Current = AssemblyCodeCache.Instance;
+            var a = new JSContext(SynchronizationContext.Current);
+            a[KeyStrings.global] = a;
+            return a;
+        }
+
+        public IJSContext Create(Uri inverseWebSocketUri)
+        {
+            // DictionaryCodeCache.Current = AssemblyCodeCache.Instance;
+            var a = new JSContext(SynchronizationContext.Current);
+            a[KeyStrings.global] = a;
+            return a;
+        }
+    }
+}
