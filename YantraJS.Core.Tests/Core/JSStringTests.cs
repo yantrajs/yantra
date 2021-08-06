fc8c1ec06cc2b47c813643079a8e44738bc4e79a
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-08-06 22:57:59 +0530

    wip

--- YantraJS.Core.Tests/Core/JSStringTests.cs
+++ YantraJS.Core.Tests/Core/JSStringTests.cs
@@ -17,20 +17,6 @@ namespace YantraJS.Tests.Core
             // Assert.AreEqual(1, context.Eval("x = {get f() { return 1; }}; x.f = 5; x.f"));
             this.context["array"] = new JSArray().Add(new JSNumber(1));
             this.context.Eval(@"
-function* g1(n) {
-    try {
-        yield 1;
-    } catch(e) {
-        yield e;
-    }
-}
-
-var g = g1(1);
-var a = g.next();
-assert.strictEqual(1, a.value);
-g.throw(new Error('2'));
-a = g.next();
-assert.strictEqual('2', a.value.toString());
 
 ");
         }
