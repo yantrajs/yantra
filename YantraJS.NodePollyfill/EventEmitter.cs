f34fc491a0d6f553e7a7185d1cf1266048e9f444
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-09-17 10:56:29 +0530

    Added node pollyfill

---
+++ YantraJS.NodePollyfill/EventEmitter.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using YantraJS.Core;
+using YantraJS.Core.Core.Storage;
+
+namespace YantraJS.NodePollyfill
+{
+
+    public class Event
+    {
+
+    }
+
+    public class EventInfo
+    {
+        public JSValue Listener;
+
+        public Action<Event> Delegate;
+
+    }
+
+    public class EventEmitter
+    {
+
+        private StringMap<List<JSValue>> listeners;
+
+        private string ToKey(JSValue value)
+            => value.IsString
+                ? "key:" + value.ToString()
+                : ((value is JSSymbol symbol)
+                    ? "symbol:" + symbol.Key
+                    : throw JSContext.CurrentContext.NewTypeError("Key can only be string or symbol"));
+
+        public EventEmitter(in Arguments a)
+        {
+
+        }
+
+        public JSValue On(in Arguments a)
+        {
+            var (eventName, listener) = a.Get2();
+            this.AddEventListener(ToKey(eventName), listener);
+            return JSUndefined.Value;
+        }
+
+        public JSValue Once(in Arguments a)
+        {
+            var (eventName, listener) = a.Get2();
+            var key = ToKey(eventName);
+
+            var task = new JSPromise((r, e) => {
+                var remove = new JSFunction((in Arguments a1) => {
+                    if (listeners.TryGetValue(key, out var list))
+                    {
+                        listener.InvokeFunction(in a1);
+                        list.Remove(listener);
+                        r(JSUndefined.Value);
+                    }
+                    return JSUndefined.Value;
+                });
+
+                AddEventListener(key, remove);
+            });
+
+            return task;
+        }
+
+        public JSValue Emit(in Arguments a)
+        {
+            var name = ToKey(a.Get1());
+
+            if(listeners.TryGetValue(name,out var list))
+            {
+
+                var e = new Event();
+                var jse = e.Marshal();
+                foreach(var item in list)
+                {
+                    item.InvokeFunction(new Arguments(JSUndefined.Value, jse));
+                }
+            }
+
+            return JSUndefined.Value;
+        }
+
+        private void AddEventListener(string name, JSValue handler)
+        {
+            if(!listeners.TryGetValue(name, out var list))
+            {
+                list = new List<JSValue>();
+                listeners.Save(name, list);
+            }
+
+            list.Add(handler);
+        }
+
+        public JSValue AddListener(in Arguments a) => On(in a);
+
+    }
+}
--- modules/inbuilt/events.csx
+++ modules/inbuilt/events.csx
@@ -1,4 +1,5 @@
 #r "nuget: YantraJS.Core,1.1.106"
+#r "nuget: YantraJS.NodePollyfill,1.1.107"
 using System;
 using System.Linq;
 using System.Collections.Generic;
@@ -7,42 +8,7 @@ using YantraJS.Core.Clr;
 using YantraJS.Core.Core.Storage;
 
 
-
-public class Event {
-
-}
-
-public class EventInfo {
-    public JSValue Listener;
-
-    public Action<Event> Delegate;
-
-}
-
 [Export]
-public class EventEmitter {
-
-    private StringMap<List<JSValue>> listeners;
-
-    private string ToKey(JSValue value)
-        => value.IsString
-            ? "key:" + value.ToString()
-            : ((value is JSSymbol symbol) 
-                ? "symbol:" + symbol.Key
-                : throw JSContext.CurrentContext.NewTypeError("Key can only be string or symbol"));
-
-    public EventEmitter(in Arguments a)
-    {
-        
-    }
-
-    public JSValue On(in Arguments a) {
-
-        var name = ToKey(a.Get1());
-        this.AddEventListener(name, (e) => a.Get2().InvokeFunction(new Arguments(JSUndefined.Value, e.Marshal() )));
-        return JSUndefined.Value;
-    }
-
-    public JSValue AddListener(in Arguments a) => On(in a);
+public class EventEmitter: YantraJS.NodePollyfill.EventEmitter {
 
 }
