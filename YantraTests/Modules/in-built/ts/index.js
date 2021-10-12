4466d56d8c0e091ab27075d6307efe32f127eaf4
Akash Kava <39438041+ackava@users.noreply.github.com>
2021-10-12 11:28:30 +0530

    Sequence wip

--- YantraTests/Modules/in-built/ts/index.js
+++
@@ -1,8 +0,0 @@
-﻿////// import * as ts from "./typescript";
-//var ts = require("./typescript");
-//console.log("Typescript module loaded...");
-//const source = "let x: string  = 'string'";
-
-//let result = ts.transpileModule(source, { compilerOptions: { module: ts.ModuleKind.CommonJS } });
-
-//console.log(JSON.stringify(result));
