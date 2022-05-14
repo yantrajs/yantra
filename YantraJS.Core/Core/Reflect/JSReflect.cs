16ed5a6bc7c9d11a7db471d9960e5b3c1c57fdd0
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-05-14 22:31:54 +0530

    Reflect in progress

---
+++ YantraJS.Core/Core/Reflect/JSReflect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YantraJS.Core.Internal
+{
+    public class JSReflectStatic
+    {
+
+        [Static("apply")]
+        public static JSValue Apply(in Arguments a)
+        {
+            if(!(a[0] is JSFunction fx))
+            {
+                throw JSContext.Current.NewTypeError("Function expected");
+            }
+            return JSUndefined.Value;
+        }
+
+    }
+}
