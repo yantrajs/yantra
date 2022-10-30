8ffb3ab143c533f71569ce00bf89f0bec502eb4d
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-10-30 21:16:01 +0530

    Export const fixed

--- YantraTests/Modules/in-built/exports/index.js
+++ YantraTests/Modules/in-built/exports/index.js
@@ -1,4 +1,6 @@
 ﻿
-var { mul, default: a } = require("./a");
+var { mul, default: a, log } = require("./a");
 assert.strictEqual("A B", a("A", "B"));
 assert.strictEqual(12, mul(3, 4));
+
+log("a");
