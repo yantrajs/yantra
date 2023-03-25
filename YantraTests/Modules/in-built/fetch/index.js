6cbbad8502f22b8d8403ae12ed88185dbe43fb62
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-03-25 20:35:25 +0530

    Added unit test for fetch

---
+++ YantraTests/Modules/in-built/fetch/index.js
@@ -0,0 +1,8 @@
+﻿async function test() {
+    const rs = await fetch("https://httpbin.org/get");
+    const json = await rs.json();
+
+    assert.strictEqual(json.headers.Host, "httpbin.org");
+}
+
+await test();
