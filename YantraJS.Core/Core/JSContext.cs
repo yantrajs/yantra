82eb40bf4fc1f8db6c1d0c944b901d6dcd5e9273
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-04-07 21:29:12 +0530

    BigInt moved go Generated Class

--- YantraJS.Core/Core/JSContext.cs
+++ YantraJS.Core/Core/JSContext.cs
@@ -236,7 +236,7 @@ namespace YantraJS.Core
 
         internal readonly JSObject GeneratorPrototype;
 
-        internal readonly JSObject BigIntPrototype;
+        // internal readonly JSObject BigIntPrototype;
 
         // public readonly JSObject ArrayBufferPrototype;
 
@@ -444,7 +444,7 @@ namespace YantraJS.Core
             // WeakSetPrototype = this.Create<JSWeakSet>(KeyStrings.WeakSet).prototype;
             // WeakMapPrototype = this.Create<JSWeakMap>(KeyStrings.WeakMap).prototype;
             GeneratorPrototype = this.Create<JSGenerator>(KeyStrings.Generator).prototype;
-            BigIntPrototype = this.Create<JSBigInt>(KeyStrings.BigInt).prototype;
+            // BigIntPrototype = this.Create<JSBigInt>(KeyStrings.BigInt).prototype;
             // ArrayBufferPrototype = this.Create<JSArrayBuffer>(KeyStrings.ArrayBuffer).prototype;
             Int8ArrayPrototype = this.Create<Int8Array>(KeyStrings.Int8Array).prototype;
             Uint8ArrayPrototype = this.Create<Uint8Array>(KeyStrings.Uint8Array).prototype;
