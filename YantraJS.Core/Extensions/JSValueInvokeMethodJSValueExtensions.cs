4921fe1bde8952092a8b36326ecafe0f487a1fce
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-09-12 10:07:20 +0530

    Added parser

--- YantraJS.Core/Extensions/JSValueInvokeMethodJSValueExtensions.cs
+++ YantraJS.Core/Extensions/JSValueInvokeMethodJSValueExtensions.cs
@@ -71,6 +71,67 @@ namespace YantraJS.Core
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static JSValue InvokeMethod(this JSValue @this, JSSymbol name)
+        {
+            var fx = @this[name];
+            if (fx.IsUndefined)
+                throw JSContext.Current.NewTypeError($"Method {name} not found on {@this}");
+            var a = new Arguments(@this);
+            return fx.InvokeFunction(a);
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static JSValue InvokeMethod(this JSValue @this, JSSymbol name, JSValue arg0)
+        {
+            var fx = @this[name];
+            if (fx.IsUndefined)
+                throw JSContext.Current.NewTypeError($"Method {name} not found on {@this}");
+            var a = new Arguments(@this, arg0);
+            return fx.InvokeFunction(a);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static JSValue InvokeMethod(this JSValue @this, JSSymbol name, JSValue arg0, JSValue arg1)
+        {
+            var fx = @this[name];
+            if (fx.IsUndefined)
+                throw JSContext.Current.NewTypeError($"Method {name} not found on {@this}");
+            var a = new Arguments(@this, arg0, arg1);
+            return fx.InvokeFunction(a);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static JSValue InvokeMethod(this JSValue @this, JSSymbol name, JSValue arg0, JSValue arg1, JSValue arg2)
+        {
+            var fx = @this[name];
+            if (fx.IsUndefined)
+                throw JSContext.Current.NewTypeError($"Method {name} not found on {@this}");
+            var a = new Arguments(@this, arg0, arg1, arg2);
+            return fx.InvokeFunction(a);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static JSValue InvokeMethod(this JSValue @this, JSSymbol name, JSValue arg0, JSValue arg1, JSValue arg2, JSValue arg3)
+        {
+            var fx = @this[name];
+            if (fx.IsUndefined)
+                throw JSContext.Current.NewTypeError($"Method {name} not found on {@this}");
+            var a = new Arguments(@this, arg0, arg1, arg2, arg3);
+            return fx.InvokeFunction(a);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static JSValue InvokeMethod(this JSValue @this, JSSymbol name, JSValue[] args)
+        {
+            var fx = @this[name];
+            if (fx.IsUndefined)
+                throw JSContext.Current.NewTypeError($"Method {name} not found on {@this}");
+            var a = new Arguments(@this, args);
+            return fx.InvokeFunction(a);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static JSValue InvokeMethodSpread(this JSValue @this, JSValue name, JSValue[] args)
         {
             var fx = @this[name];
