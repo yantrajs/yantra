6a7942ea20cf863d02ed64c86870deab05f7607e
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-10-21 19:45:56 +0530

    Added support for stack overflow

---
+++ YantraJS.Core.Tests/Generator/Files/es5/Function/stack-overflow.js
@@ -0,0 +1,17 @@
+﻿let i = 0;
+function recursive() {
+    i++;
+    recursive();
+}
+
+try {
+    recursive();
+} catch (error) {
+    if (error instanceof RangeError) {
+        if (/maximum\scall\sstack/i.test(error.message)) {
+            console.log(`Failed after ${i} recursive calls`);
+            return;
+        }
+    }
+    throw new Error("Stack overflow expected");
+}
---
+++ YantraJS.Core/Core/JSContextExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace YantraJS.Core.Core
+{
+    public static class JSContextExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void EnsureSufficientExecutionStack(this JSContext context)
+        {
+#if NETSTANDARD2_1_OR_GREATER
+            if(RuntimeHelpers.TryEnsureSufficientExecutionStack())
+            {
+                return;
+            }
+#else
+            try
+            {
+                RuntimeHelpers.EnsureSufficientExecutionStack();
+            }
+            catch (Exception ex)
+            {
+                if (ex is not InsufficientExecutionStackException)
+                {
+                    throw ex;
+                }
+            }
+#endif
+            throw context.NewRangeError("Maximum call stack size exceeded");
+        }
+
+
+    }
+}
