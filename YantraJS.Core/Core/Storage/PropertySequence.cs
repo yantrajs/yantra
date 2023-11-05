be90c351c96cf4b428f42314f97a5ef2dba37525
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-11-05 12:22:23 +0530

    wip

--- YantraJS.Core/Core/Storage/PropertySequence.cs
+++ YantraJS.Core/Core/Storage/PropertySequence.cs
@@ -18,41 +18,6 @@ namespace YantraJS.Core
 
     public delegate void Updater<TKey, TValue>(TKey key, ref TValue value);
 
-    public struct RefList<T>
-    {
-        const int Size = 16;
-
-        private T[] array;
-        private int last;
-        public int Length => last;
-
-        public ref T Put()
-        {
-            if (array == null)
-            {
-                array = new T[Size];
-            } else if (last == array.Length) 
-            {
-                Array.Resize(ref array, array.Length + Size);
-            }
-            return ref array[last++];
-        }
-
-        public void Remove(T item)
-        {
-            var index = Array.IndexOf(array, item);
-            if (index == -1)
-            {
-                return;
-            }
-            while(index < last - 1)
-            {
-                array[last] = array[last + 1];
-            }
-            last--;
-        }
-    }
-
     public struct PropertySequence
     {
 
---
+++ YantraJS.Core/Core/Storage/RefList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace YantraJS.Core
+{
+    public struct RefList<T>
+    {
+        const int Size = 16;
+
+        private T[] array;
+        private int last;
+        public int Length => last;
+
+        public ref T Put()
+        {
+            if (array == null)
+            {
+                array = new T[Size];
+            } else if (last == array.Length) 
+            {
+                Array.Resize(ref array, array.Length + Size);
+            }
+            return ref array[last++];
+        }
+
+        public void Remove(T item)
+        {
+            var index = Array.IndexOf(array, item);
+            if (index == -1)
+            {
+                return;
+            }
+            while(index < last - 1)
+            {
+                array[last] = array[last + 1];
+            }
+            last--;
+        }
+    }
+}
