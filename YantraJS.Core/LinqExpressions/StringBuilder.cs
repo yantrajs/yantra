062d7653dec769f54d528c8667fed01b325e0890
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-02-09 17:23:24 +0530

    Methods optimized

--- YantraJS.Core/LinqExpressions/StringBuilder.cs
+++ YantraJS.Core/LinqExpressions/StringBuilder.cs
@@ -10,39 +10,42 @@ using LabelTarget = YantraJS.Expressions.YLabelTarget;
 using SwitchCase = YantraJS.Expressions.YSwitchCaseExpression;
 using GotoExpression = YantraJS.Expressions.YGoToExpression;
 using TryExpression = YantraJS.Expressions.YTryCatchFinallyExpression;
+using YantraJS.Core.LambdaGen;
 namespace YantraJS.ExpHelper
 {
     public class ClrStringBuilder
 
     {
-        private static Type type = typeof(string);
+        //private static Type type = typeof(string);
 
-        private static MethodInfo _Compare =
-            type.StaticMethod(nameof(String.Compare), typeof(string), typeof(string));
+        //private static MethodInfo _Compare =
+        //    type.StaticMethod(nameof(String.Compare), typeof(string), typeof(string));
 
-        private static MethodInfo _Equals =
-            type.PublicMethod(nameof(String.Equals), typeof(string));
+        //private static MethodInfo _Equals =
+        //    type.PublicMethod(nameof(String.Equals), typeof(string));
+
+        //private static MethodInfo _Concat =
+        //    type.StaticMethod(nameof(String.Concat), typeof(string), typeof(string));
+
 
         public static Expression Equal(Expression left, Expression right)
         {
             // return Expression.Equal( Expression.Call(null, _Compare, left, right), Expression.Constant(0) );
-            return Expression.Call(left, _Equals, right);
+            // return Expression.Call(left, _Equals, right);
+            return left.CallExpression<string, bool>(() => (x) => x.Equals(""), right);
         }
         public static Expression NotEqual(Expression left, Expression right)
         {
             return Expression.Not(Equal(left,right));
         }
-        public static Expression Compare(Expression left, Expression right)
-        {
-            return Expression.Call(null, _Compare, left, right);
-        }
-
-        private static MethodInfo _Concat =
-            type.StaticMethod(nameof(String.Concat), typeof(string), typeof(string));
 
         public static Expression Concat(Expression left, Expression right)
         {
-            return Expression.Call(null, _Concat, left, right);
+            return NewLambdaExpression.StaticCallExpression(() =>
+                () => String.Concat("", "")
+                , left
+                , right);
+            // return Expression.Call(null, _Concat, left, right);
         }
 
     }
