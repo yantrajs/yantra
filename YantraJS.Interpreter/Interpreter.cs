a896fd78d8133e2c1df4df3679d82af8f822b4c0
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-11-25 22:13:48 +0530

    Adding interpreter

---
+++ YantraJS.Interpreter/Interpreter.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using YantraJS.Core;
+using YantraJS.Core.FastParser;
+using YantraJS.Utils;
+
+namespace YantraJS.Interpreter;
+
+public class Interpreter
+{
+
+}
+
+public enum ExecutionState { 
+    None,
+    Executing,
+    Yield
+};
+
+public class FunctionScope
+{
+    public string Calle { get; set; }
+
+    public SAUint32Map<JSValue> Variables;
+
+    public Arguments Arguments;
+
+    public FunctionScope Caller { get; set; }
+
+
+}
+
+public delegate JSValue ExecuteStep(FunctionScope scope);
+
+public class AstToStepVisitor : AstMapVisitor<ExecuteStep>
+{
+    protected override ExecuteStep VisitArrayExpression(AstArrayExpression arrayExpression)
+    {
+        var fe = arrayExpression.Elements.GetFastEnumerator();
+        List<ExecuteStep> sequence = new();
+        while(fe.MoveNext(out var item))
+        {
+            sequence.Add(this.Visit(item));
+        }
+        return (fc) => new JSArray(sequence.Select(x => x(fc)));
+    }
+
+    protected override ExecuteStep VisitArrayPattern(AstArrayPattern arrayPattern)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitAwaitExpression(AstAwaitExpression node)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitBinaryExpression(AstBinaryExpression binaryExpression)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitBlock(AstBlock block)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitBreakStatement(AstBreakStatement breakStatement)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitCallExpression(AstCallExpression callExpression)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitClassStatement(AstClassExpression classStatement)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitConditionalExpression(AstConditionalExpression conditionalExpression)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitContinueStatement(AstContinueStatement continueStatement)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitDebuggerStatement(AstDebuggerStatement debuggerStatement)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitDoWhileStatement(AstDoWhileStatement doWhileStatement, string label = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitEmptyExpression(AstEmptyExpression emptyExpression)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitExportStatement(AstExportStatement astExportStatement)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitExpressionStatement(AstExpressionStatement expressionStatement)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitForInStatement(AstForInStatement forInStatement, string label = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitForOfStatement(AstForOfStatement forOfStatement, string label = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitForStatement(AstForStatement forStatement, string label = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitFunctionExpression(AstFunctionExpression functionExpression)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitIdentifier(AstIdentifier identifier)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitIfStatement(AstIfStatement ifStatement)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitImportStatement(AstImportStatement astImportStatement)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitLabeledStatement(AstLabeledStatement labeledStatement)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitLiteral(AstLiteral literal)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitMemberExpression(AstMemberExpression memberExpression)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitMeta(AstMeta astMeta)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitNewExpression(AstNewExpression newExpression)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitObjectLiteral(AstObjectLiteral objectLiteral)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitObjectPattern(AstObjectPattern objectPattern)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitProgram(AstProgram program)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitReturnStatement(AstReturnStatement returnStatement)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitSequenceExpression(AstSequenceExpression sequenceExpression)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitSpreadElement(AstSpreadElement spreadElement)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitSwitchStatement(AstSwitchStatement switchStatement)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitTaggedTemplateExpression(AstTaggedTemplateExpression astTaggedTemplateExpression)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitTemplateExpression(AstTemplateExpression templateExpression)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitThrowStatement(AstThrowStatement throwStatement)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitTryStatement(AstTryStatement tryStatement)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitUnaryExpression(AstUnaryExpression unaryExpression)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitVariableDeclaration(AstVariableDeclaration variableDeclaration)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitWhileStatement(AstWhileStatement whileStatement, string label = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ExecuteStep VisitYieldExpression(AstYieldExpression yieldExpression)
+    {
+        throw new NotImplementedException();
+    }
+}
