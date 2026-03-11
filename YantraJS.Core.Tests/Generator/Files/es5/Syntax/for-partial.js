7f0aa4d21bceee5911b6f41bb8255ad86e1906cd
Akash Kava <39438041+ackava@users.noreply.github.com>
2026-03-11 16:28:05 +0530

    added unit tests

---
+++ YantraJS.Core.Tests/Generator/Files/es5/Syntax/for-partial.js
@@ -0,0 +1,30 @@
+﻿function a(n) {
+    var s = "";
+    for (var i = 0; i < n; i++) {
+        s += i;
+    }
+    return s;
+}
+
+assert.strictEqual("012", a(3));
+
+// let...
+(function () {
+
+    var b = [];
+    for (var i = 0; i < 3; i++) {
+        b.push(() => i);
+    }
+    b = b.map((n) => n());
+    assert.strictEqual("3,3,3", b.toString());
+    b = [];
+    for (let i = 0; i < 3; i++) {
+        b.push(() => i);
+    }
+    b = b.map((n) => n());
+    assert.strictEqual("0,1,2", b.toString());
+})();
+var b = [];
+for (let i = 0; i < 3; i++) {
+    b.push(() => i);
+}
--- YantraJS.Core.Tests/Generator/Files/es5/Syntax/for.js
+++ YantraJS.Core.Tests/Generator/Files/es5/Syntax/for.js
@@ -1,30 +1,35 @@
-﻿function a(n) {
-    var s = "";
-    for (var i = 0; i < n; i++) {
-        s += i;
+﻿var a = 1;
+for (; ;) {
+    a++;
+    if (a === 10) {
+        break;
     }
-    return s;
+}
+assert.strictEqual(10, a);
+
+class A {
+
+    init() {
+        this.a = 10;
+    }
+
+    check() {
+        this.a++ < 10;
+    }
+
+    run() {
+
+        for (this.init(); this.check();) {
+            this.nothing();
+        }
+        assert.strictEqual(this.a, 11);
+    }
+
+    nothing() {
+
+    }
+
 }
 
-assert.strictEqual("012", a(3));
-
-// let...
-(function () {
-
-    var b = [];
-    for (var i = 0; i < 3; i++) {
-        b.push(() => i);
-    }
-    b = b.map((n) => n());
-    assert.strictEqual("3,3,3", b.toString());
-    b = [];
-    for (let i = 0; i < 3; i++) {
-        b.push(() => i);
-    }
-    b = b.map((n) => n());
-    assert.strictEqual("0,1,2", b.toString());
-})();
-var b = [];
-for (let i = 0; i < 3; i++) {
-    b.push(() => i);
-}
+var a1 = new A();
+a1.run();
