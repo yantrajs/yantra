725716ce5c72906df84d29d2ba377bfc01d7c1d4
Akash Kava <39438041+ackava@users.noreply.github.com>
2026-09-06 14:38:47 +0530

    Tests reorganized

--- YantraJS.Core.Tests/Generator/FilesTest.cs
+++ YantraJS.Core.Tests/Generator/FilesTest.cs
@@ -1,208 +1,208 @@
-﻿using Microsoft.Threading;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using YantraJS.Core;
-using YantraJS.Emit;
-using YantraJS.Utils;
-namespace YantraJS.Tests.Generator
-{
+﻿//using Microsoft.Threading;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using System;
+//using System.Collections.Generic;
+//using System.Data;
+//using System.Diagnostics;
+//using System.IO;
+//using System.Linq;
+//using System.Reflection;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using YantraJS.Core;
+//using YantraJS.Emit;
+//using YantraJS.Utils;
+//namespace YantraJS.Tests.Generator
+//{
 
 
-    [TestClass]
-    public class ES5
-    {
-        [TestFolder("es5\\Objects")]
-        public void Objects()
-        {
+//    [TestClass]
+//    public class ES5
+//    {
+//        [TestFolder("es5\\Objects")]
+//        public void Objects()
+//        {
             
-        }
+//        }
 
 
-        [TestFolder("es5\\Objects\\Date")]
-        public void Date()
-        {
+//        [TestFolder("es5\\Objects\\Date")]
+//        public void Date()
+//        {
 
-        }
+//        }
 
-    }
+//    }
 
-    [TestClass]
-    public class Statements
-    {
-        [TestFolder("es5\\Statements")]
-        public void Run()
-        {
+//    [TestClass]
+//    public class Statements
+//    {
+//        [TestFolder("es5\\Statements")]
+//        public void Run()
+//        {
 
-        }
+//        }
 
-    }
+//    }
 
-    [TestClass]
-    public class Syntax
-    {
-        [TestFolder("es5\\Syntax")]
-        public void Run()
-        {
+//    [TestClass]
+//    public class Syntax
+//    {
+//        [TestFolder("es5\\Syntax")]
+//        public void Run()
+//        {
 
-        }
+//        }
 
-    }
-
-
-    [TestClass]
-    public class Function
-    {
-        [TestFolder("es5\\Function")]
-        public void Run()
-        {
-
-        }
-    }
-
-    [TestClass]
-    public class ES6
-    {
-        [TestFolder("es6\\Syntax")]
-        public void Syntax()
-        {
-
-        }
-
-    }
-
-    [TestClass]
-    public class ES2023
-    {
-        [TestFolder("es\\2023")]
-        public void Disposables()
-        {
-
-        }
-
-    }
-
-    [TestClass]
-    public class ES2025
-    {
-        [TestFolder("es\\2025\\decimal")]
-        public void Decimals()
-        {
-
-        }
-
-    }
-
-    [TestClass]
-    public class Modules
-    {
-        [ModuleFolder("es6\\Modules\\clr")]
-        public void Clr()
-        {
-
-        }
-
-    }
+//    }
 
 
+//    [TestClass]
+//    public class Function
+//    {
+//        [TestFolder("es5\\Function")]
+//        public void Run()
+//        {
 
-    [TestClass]
-    public class String
-    {
-        [TestFolder("es5\\String")]
-        public void Run()
-        {
+//        }
+//    }
+
+//    [TestClass]
+//    public class ES6
+//    {
+//        [TestFolder("es6\\Syntax")]
+//        public void Syntax()
+//        {
+
+//        }
+
+//    }
+
+//    [TestClass]
+//    public class ES2023
+//    {
+//        [TestFolder("es\\2023")]
+//        public void Disposables()
+//        {
+
+//        }
+
+//    }
+
+//    [TestClass]
+//    public class ES2025
+//    {
+//        [TestFolder("es\\2025\\decimal")]
+//        public void Decimals()
+//        {
+
+//        }
+
+//    }
+
+//    [TestClass]
+//    public class Modules
+//    {
+//        [ModuleFolder("es6\\Modules\\clr")]
+//        public void Clr()
+//        {
+
+//        }
+
+//    }
+
+
+
+//    [TestClass]
+//    public class String
+//    {
+//        [TestFolder("es5\\String")]
+//        public void Run()
+//        {
             
-        }
-    }
+//        }
+//    }
 
-    [TestClass]
-    public class Array
-    {
-        [TestFolder("es5\\Array")]
-        public void Run()
-        {
+//    [TestClass]
+//    public class Array
+//    {
+//        [TestFolder("es5\\Array")]
+//        public void Run()
+//        {
 
-        }
-    }
+//        }
+//    }
 
-    [TestClass]
-    public class Number
-    {
-        [TestFolder("es5\\Number")]
-        public void Run()
-        {
+//    [TestClass]
+//    public class Number
+//    {
+//        [TestFolder("es5\\Number")]
+//        public void Run()
+//        {
 
-        }
-    }
+//        }
+//    }
 
-    [TestClass]
-    public class Math
-    {
-        [TestFolder("es5\\Math")]
-        public void Run()
-        {
+//    [TestClass]
+//    public class Math
+//    {
+//        [TestFolder("es5\\Math")]
+//        public void Run()
+//        {
 
-        }
-    }
+//        }
+//    }
 
-    public class TestContextTraceListener : TraceListener
-    {
-        private TestContext value;
+//    public class TestContextTraceListener : TraceListener
+//    {
+//        private TestContext value;
 
-        public TestContextTraceListener(TestContext value)
-        {
-            this.value = value;
-        }
+//        public TestContextTraceListener(TestContext value)
+//        {
+//            this.value = value;
+//        }
 
-        public override void Write(string message)
-        {
-            value.Write(message);
-        }
+//        public override void Write(string message)
+//        {
+//            value.Write(message);
+//        }
 
-        public override void WriteLine(string message)
-        {
-            value.WriteLine(message);
-        }
-    }
+//        public override void WriteLine(string message)
+//        {
+//            value.WriteLine(message);
+//        }
+//    }
 
-    [TestClass]
-    public class Promise
-    {
+//    [TestClass]
+//    public class Promise
+//    {
 
-        public TestContext _testContext;
+//        public TestContext _testContext;
 
-        public TestContext TestContext
-        {
-            get => _testContext;
-            set
-            {
-                _testContext = value;
-                Trace.Listeners.Add(new TestContextTraceListener(value));
-                value.WriteLine("Test context set");
-            }
-        }
+//        public TestContext TestContext
+//        {
+//            get => _testContext;
+//            set
+//            {
+//                _testContext = value;
+//                Trace.Listeners.Add(new TestContextTraceListener(value));
+//                value.WriteLine("Test context set");
+//            }
+//        }
 
-        // [AsyncTestFolder("es5\\Promise")]
-        public void Run()
-        {
+//        // [AsyncTestFolder("es5\\Promise")]
+//        public void Run()
+//        {
 
-        }
+//        }
 
-        [AsyncTestFolder("es6\\Async")]
-        public void RunAsync()
-        {
+//        // [AsyncTestFolder("es6\\Async")]
+//        public void RunAsync()
+//        {
 
-        }
+//        }
 
-    }
+//    }
 
-}
+//}
---
+++ YantraJS.Core.Tests/Generator/ScriptFileTest.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using YantraJS.Emit;
+using YantraJS.Tests;
+using YantraJS.Utils;
+
+namespace YantraJS.Core.ScriptTests;
+
+[TestClass]
+public class ScriptFileTest
+{
+    private static readonly string folder = "../../../Generator/Files/";
+
+    public static IEnumerable<object[]> GetJavaScriptTestFiles()
+    {
+        // Return the full file path to the test method
+        return Directory.GetFiles(folder, "*.js", SearchOption.AllDirectories)
+                        .Select(filePath => new object[] {
+                            filePath
+                        });
+    }
+
+    public static string GetCustomTestName(MethodInfo methodInfo, object[] data)
+    {
+        if (data != null && data.Length > 0 && data[0] is string filePath)
+        {
+            // Returns just "my-script.js" or "folder_subfolder_script.js" 
+            var name = Path.GetRelativePath(folder, filePath)
+                    .Replace("\\", "/");
+            return name;
+        }
+        return methodInfo.Name;
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(GetJavaScriptTestFiles), DynamicDataDisplayName = nameof(GetCustomTestName))]
+    public async Task ExecuteScript(string filePath)
+    {
+        await RunAsyncTest(new FileInfo(filePath));
+    }
+
+    protected async Task RunAsyncTest(FileInfo file)
+    {
+
+        // var watch = new Stopwatch();
+        // watch.Start();
+        var old = SynchronizationContext.Current;
+        try
+        {
+            var ctx = new SynchronizationContext();
+            SynchronizationContext.SetSynchronizationContext(ctx);
+            Exception lastError = null;
+            System.Diagnostics.Debug.WriteLine($"Processing {file.FullName}");
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                string content;
+                using (var fs = file.OpenText())
+                {
+                    content = await fs.ReadToEndAsync();
+                }
+                using (var jc = CreateContext(file, ctx))
+                {
+                    jc.Log += (_, s) =>
+                    {
+                        var text = s.ToDetailString();
+                        Console.WriteLine(text);
+                    };
+                    jc.Error += (_, e) => lastError = e;
+                    await EvaluateAsync(jc, content, file.FullName);
+                }
+            }
+            catch (Exception ex)
+            {
+                lastError = ex;
+            }
+        }
+        finally
+        {
+            SynchronizationContext.SetSynchronizationContext(old);
+        }
+    }
+
+    protected virtual JSContext CreateContext(FileInfo file, SynchronizationContext ctx)
+    {
+        return new JSTestContext(ctx);
+    }
+
+    protected virtual Task EvaluateAsync(JSContext context, string content, string fullName)
+    {
+        return CoreScript.EvaluateAsync(content, fullName, DictionaryCodeCache.Current);
+
+    }
+}
